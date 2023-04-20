908077c57ed2c1c29e9553df738ca1330ac6c95f
pnlt <pnlt170603@gmail.com>
2023-04-20 11:24:12 +0700

    feat(*): initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  extends: ["plugin:react/recommended", "prettier"],
+  plugins: ["prettier"],
+  rules: {
+    "prettier/prettier": ["error", {}, { usePrettierrc: true }],
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: "module",
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+};
